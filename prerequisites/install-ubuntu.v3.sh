007af1f1c9027184fa2b49e22861788971209d19
DOMCSC <56617942+DOMGO-cut@users.noreply.github.com>
2024-07-27 05:04:55 +0800

    Update install-ubuntu.v3.sh

--- prerequisites/install-ubuntu.v3.sh
+++ prerequisites/install-ubuntu.v3.sh
@@ -25,12 +25,12 @@ INSTALL_DIR="/opt/postal/install"
 SYMLINK="/usr/bin/postal"
 
 if [ -d "$INSTALL_DIR" ]; then
-  echo -e "\e[35mstart\e[0m 目标目录 $INSTALL_DIR 已存在，正在删除..."
+  echo -e "\e[35mPostal 安装\e[0m 目标目录 $INSTALL_DIR 已存在，正在删除..."
   rm -rf "$INSTALL_DIR"
 fi
 
 if [ -L "$SYMLINK" ]; then
-  echo -e "\e[35mstart\e[0m 符号链接 $SYMLINK 已存在，正在删除..."
+  echo -e "\e[35mPostal 安装\e[0m 符号链接 $SYMLINK 已存在，正在删除..."
   rm -rf "$SYMLINK"
 fi
 
@@ -39,11 +39,11 @@ git clone https://github.com/DOMGO-cut/postal.git "$INSTALL_DIR"
 ln -s "$INSTALL_DIR/bin/postal" "$SYMLINK"
 
 sudo chmod +x "$INSTALL_DIR/bin/postal"
-echo -e "\e[35mstart\e[0m 权限已执行完毕"
+echo -e "\e[35mPostal 安装\e[0m 权限已执行完毕"
 
 # 删除现有的 MariaDB 容器（如果存在）
 if [ "$(docker ps -aq -f name=postal-mariadb)" ]; then
-  echo -e "\e[35mstart\e[0m 现有的 MariaDB 容器存在，正在删除..."
+  echo -e "\e[35mPostal 安装\e[0m 现有的 MariaDB 容器存在，正在删除..."
   docker rm -f postal-mariadb
 fi
 
@@ -57,32 +57,32 @@ docker run -d \
    mariadb
 
 # 提示用户输入域名
-echo -e "\e[35mstart\e[0m 请输入你的域名（例如: example.com）:"
+echo -e "\e[35mPostal 安装\e[0m 请输入你的域名（例如: example.com）:"
 read domain
 
 # 检查用户是否输入了域名
 if [ -z "$domain" ]; then
-  echo -e "\e[35mstart\e[0m 缺少主机名。请确保输入一个有效的域名。"
+  echo -e "\e[35mPostal 安装\e[0m 缺少主机名。请确保输入一个有效的域名。"
   exit 1
 fi
 
 # 运行 postal bootstrap 命令
 postal bootstrap "$domain"
 
-echo -e "\e[35mstart\e[0m Postal bootstrap 已执行完毕，使用域名: $domain"
+echo -e "\e[35mPostal 安装\e[0m Postal bootstrap 已执行完毕，使用域名: $domain"
 
-echo -e "\e[35mstart\e[0m 正在进行初始化数据库"
+echo -e "\e[35mPostal 安装\e[0m 正在进行初始化数据库"
 postal initialize
 
 postal make-user
-echo -e "\e[35mstart\e[0m 数据库初始化完毕"
+echo -e "\e[35mPostal 安装\e[0m 数据库初始化完毕"
 
 postal start
-echo -e "\e[35mstart\e[0m 开启 postal 服务成功"
+echo -e "\e[35mPostal 安装\e[0m 开启 postal 服务成功"
 
 # 删除现有的 Caddy 容器（如果存在）
 if [ "$(docker ps -aq -f name=postal-caddy)" ]; then
-  echo -e "\e[35mstart\e[0m 现有的 Caddy 容器存在，正在删除..."
+  echo -e "\e[35mPostal 安装\e[0m 现有的 Caddy 容器存在，正在删除..."
   docker rm -f postal-caddy
 fi
 
@@ -102,23 +102,16 @@ ipv4=$(curl -s https://api64.ipify.org)
 # 将结果赋值给变量
 ips="$ipv4"
 
-# 获取默认 DKIM 记录
-dkim_output=$(postal default-dkim-record)
-
-# 提取 DKIM 记录（假设输出格式是纯文本的 DKIM 记录）
-# 如果输出有特定的格式，例如 JSON，你可能需要使用 `jq` 或其他工具来解析
-DKIM="$dkim_output"
 
 echo -e "\e[35mPostal 安装\e[0m 安装完成，请打开网址访问 postal 服务，https://$domain"
 echo ""
 echo -e "\e[35mPostal 安装\e[0m 设置A记录为： @ $ips"
-
-
+echo ""
 echo -e "\e[35mDNS配置\e[0m 设置MX记录为：   \e[32m    rp \e[0m       \e[31m MX   10 \e[0m  $domain"
 echo ""
-echo -e "\e[35mDNS配置\e[0m 设置MX记录为：   \e[32m    @ \e[0m        \e[31m MX   10  \e[0m mx.$domain"
+echo -e "\e[35mDNS配置\e[0m 设置MX记录为：   \e[32m    @ \e[0m        \e[31m MX   10 \e[0m mx.$domain"
 echo ""
-echo -e "\e[35mDNS配置\e[0m 设置返回MX记录为：\e[32m routes \e[0m  \e[31m MX  10 e[0m  $domain"
+echo -e "\e[35mDNS配置\e[0m 设置返回MX记录为：\e[32m routes \e[0m  \e[31m MX  10 \e[0m  $domain"
 echo ""
 echo -e "\e[35mDNS配置\e[0m 设置DMARC记录为： \e[32m _dmarc \e[0m \e[31m TXT \e[0m  v=DMARC1;p=quarantine;rua=mailto:admin@$domain"
 echo ""
