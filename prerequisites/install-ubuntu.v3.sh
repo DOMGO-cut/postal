349678529c1ee2d564c9bea801e0ad0512b597e4
DOMGO-cut <56617942+DOMGO-cut@users.noreply.github.com>
2024-07-26 23:26:05 +0800

    Update install-ubuntu.v3.sh

--- prerequisites/install-ubuntu.v3.sh
+++ prerequisites/install-ubuntu.v3.sh
@@ -52,7 +52,7 @@ docker run -d \
    --name postal-mariadb \
    -p 127.0.0.1:3306:3306 \
    --restart always \
-   -e MARIADB_DATABASE=domcsc \
+   -e MARIADB_DATABASE=postal \
    -e MARIADB_ROOT_PASSWORD=hzx19960426 \
    mariadb
 
