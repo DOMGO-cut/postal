5b57ccdb68cfe0a1cea0bd7e93d59275dcb616b7
DOMGO-cut <56617942+DOMGO-cut@users.noreply.github.com>
2024-07-27 00:39:37 +0800

    Update install-ubuntu.v3.sh

--- prerequisites/install-ubuntu.v3.sh
+++ prerequisites/install-ubuntu.v3.sh
@@ -122,13 +122,13 @@ echo -e "\e[35mPostal 安装e[0m rp.MX记录为：    MX   10   rp.$domain"
 
 echo -e "\e[35mPostal 安装e[0m 返回MX记录为： routes  MX  10  $domain"
 
-echo -e "\e[35mPostal 安装e[0m DMARC记录为： _dmarc   v=DMARC1;p=quarantine;rua=mailto:admin@$domain"
+echo -e "\e[35mPostal 安装e[0m DMARC记录为： _dmarc  TXT  v=DMARC1;p=quarantine;rua=mailto:admin@$domain"
 
-echo -e "\e[35mPostal 安装e[0m SPF记录为：   spf   v=spf1 a mx include:spf.$domain ~all"
+echo -e "\e[35mPostal 安装e[0m SPF记录为：   rp   TXT  v=spf1 a mx include:spf.$domain ~all"
 
-echo -e "\e[35mPostal 安装e[0m SPF记录为：   spf   v=spf1 ip4:$ips   ip6:$ipss ~all
+echo -e "\e[35mPostal 安装e[0m SPF记录为：   spf  TXT  v=spf1 ip4:$ips   ip6:$ipss ~all
 
-echo -e "\e[35mPostal 安装e[0m DKIM记录为：  default._domainkey.$domain   $DKIM"
+echo -e "\e[35mPostal 安装e[0m DKIM记录为：  postal.default._domainkey      $DKIM"
 
 echo -e "\e[35mPostal 安装e[0m 返回MX记录为： routes  MX  10  $domain"
 
