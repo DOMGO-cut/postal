6f64f8b57cf2cc8f5036412b30ab2f16082040c7
DOMCSC <56617942+DOMGO-cut@users.noreply.github.com>
2024-07-27 05:20:38 +0800

    Update install-ubuntu.v3.sh

--- prerequisites/install-ubuntu.v3.sh
+++ prerequisites/install-ubuntu.v3.sh
@@ -28,25 +28,25 @@ if [ -d "$INSTALL_DIR" ]; then
   echo -e "\e[35mPostal 安装\e[0m 目标目录 $INSTALL_DIR 已存在，正在删除..."
   rm -rf "$INSTALL_DIR"
 fi
-
+echo ""
 if [ -L "$SYMLINK" ]; then
   echo -e "\e[35mPostal 安装\e[0m 符号链接 $SYMLINK 已存在，正在删除..."
   rm -rf "$SYMLINK"
 fi
-
+echo ""
 # 克隆 Postal 仓库
 git clone https://github.com/DOMGO-cut/postal.git "$INSTALL_DIR"
 ln -s "$INSTALL_DIR/bin/postal" "$SYMLINK"
 
 sudo chmod +x "$INSTALL_DIR/bin/postal"
 echo -e "\e[35mPostal 安装\e[0m 权限已执行完毕"
-
+echo ""
 # 删除现有的 MariaDB 容器（如果存在）
 if [ "$(docker ps -aq -f name=postal-mariadb)" ]; then
   echo -e "\e[35mPostal 安装\e[0m 现有的 MariaDB 容器存在，正在删除..."
   docker rm -f postal-mariadb
 fi
-
+echo ""
 # 启动 MariaDB 容器
 docker run -d \
    --name postal-mariadb \
@@ -59,10 +59,11 @@ docker run -d \
 # 提示用户输入域名
 echo -e "\e[35mPostal 安装\e[0m 请输入你的域名（例如: example.com）:"
 read domain
-
+echo ""
 # 检查用户是否输入了域名
 if [ -z "$domain" ]; then
   echo -e "\e[35mPostal 安装\e[0m 缺少主机名。请确保输入一个有效的域名。"
+  echo ""
   exit 1
 fi
 
@@ -70,19 +71,20 @@ fi
 postal bootstrap "$domain"
 
 echo -e "\e[35mPostal 安装\e[0m Postal bootstrap 已执行完毕，使用域名: $domain"
-
+echo ""
 echo -e "\e[35mPostal 安装\e[0m 正在进行初始化数据库"
 postal initialize
-
+echo ""
 postal make-user
 echo -e "\e[35mPostal 安装\e[0m 数据库初始化完毕"
-
+echo ""
 postal start
 echo -e "\e[35mPostal 安装\e[0m 开启 postal 服务成功"
-
+echo ""
 # 删除现有的 Caddy 容器（如果存在）
 if [ "$(docker ps -aq -f name=postal-caddy)" ]; then
   echo -e "\e[35mPostal 安装\e[0m 现有的 Caddy 容器存在，正在删除..."
+  echo ""
   docker rm -f postal-caddy
 fi
 
@@ -102,14 +104,14 @@ ipv4=$(curl -s https://api64.ipify.org)
 # 将结果赋值给变量
 ips="$ipv4"
 
-
+echo ""
 echo -e "\e[35mPostal 安装\e[0m 安装完成，请打开网址访问 postal 服务，\e[31m https://$domain \e[0m"
 echo ""
-echo -e "\e[35mPostal 安装\e[0m 设置A记录为： @ $ips"
-echo ""
-echo -e "\e[35mDNS配置\e[0m 设置MX记录为：   \e[32m    rp \e[0m       \e[31m MX   10 \e[0m  $domain"
+echo -e "\e[35mPostal 安装\e[0m 设置A记录为： \e[31m   @  \e[0m       \e[31m A  \e[0m    $ips"
 echo ""
 echo -e "\e[35mDNS配置\e[0m 设置MX记录为：   \e[32m    @ \e[0m        \e[31m MX   10 \e[0m mx.$domain"
+echo ""
+echo -e "\e[35mDNS配置\e[0m 设置MX记录为：   \e[32m    rp \e[0m       \e[31m MX   10 \e[0m  $domain"
 echo ""
 echo -e "\e[35mDNS配置\e[0m 设置返回MX记录为：\e[32m routes \e[0m  \e[31m MX  10 \e[0m  $domain"
 echo ""
